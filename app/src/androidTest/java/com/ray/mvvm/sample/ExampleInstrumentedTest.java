79554bee64d00e345ba3d6ee571b4287a15188aa
Rayman.t.Yan <slyyantao@gmail.com>
2017-02-11 13:06:33 +0800

    Add test config

--- app/src/androidTest/java/com/ray/mvvm/sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ray/mvvm/sample/ExampleInstrumentedTest.java
@@ -2,12 +2,13 @@ package com.ray.mvvm.sample;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -15,12 +16,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@MediumTest
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.ray.mvvm.sample", appContext.getPackageName());
+        assertEquals(BuildConfig.APPLICATION_ID, appContext.getPackageName());
     }
+
 }
