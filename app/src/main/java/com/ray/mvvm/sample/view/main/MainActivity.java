41dfa6bf6c75f6c3b02c5c1b1624a3feef9f4258
Rayman.t.Yan <slyyantao@gmail.com>
2017-02-15 12:48:09 +0800

    Upgrade library

--- app/src/main/java/com/ray/mvvm/sample/view/main/MainActivity.java
+++ app/src/main/java/com/ray/mvvm/sample/view/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 
 import com.ray.mvvm.lib.view.base.page.BaseDIActivity;
+import com.ray.mvvm.lib.widget.lifecycle.LifecycleEvent;
 import com.ray.mvvm.sample.R;
 import com.ray.mvvm.sample.view.main.contract.DaggerMainContract_Comp;
 import com.ray.mvvm.sample.view.main.contract.MainContract;
@@ -13,18 +14,25 @@ import com.tbruyelle.rxpermissions.RxPermissions;
 
 import javax.inject.Inject;
 
+import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public final class MainActivity extends BaseDIActivity implements MainContract.View {
 
-    @Inject MainVM viewModel;
+    @Inject
+    MainVM viewModel;
+    @Inject
+    RxPermissions rxPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bindLayout(R.layout.activity_main, viewModel, false);
         viewModel.init();
-        viewModel.requestPermission(RxPermissions.getInstance(this));
+        requestPermission();
     }
 
     @Override
@@ -45,5 +53,22 @@ public final class MainActivity extends BaseDIActivity implements MainContract.V
                 .setNegativeButton("Allow", (dialogInterface, i) -> subject.onNext(true))
                 .setPositiveButton("Cancel", (dialogInterface, i) -> finish())
                 .show();
+    }
+
+    private void requestPermission() {
+        PublishSubject<Boolean> subject = PublishSubject.create();
+        subject
+                .compose(rxPermissions
+                        .ensure(WRITE_EXTERNAL_STORAGE,
+                                READ_EXTERNAL_STORAGE))
+                .compose(bindUntilEvent(LifecycleEvent.DESTROY))
+                .subscribe(granted -> {
+                    if (granted) {
+                        subject.onCompleted();
+                    } else {
+                        showPermissionDialog(subject);
+                    }
+                });
+        subject.onNext(true);
     }
 }
